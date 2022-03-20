b7d1bd1722f117a4f939b0a38e2a2445124098d6
shawasty <shawasty@gmail.com>
2022-03-19 23:34:47 -0400

    added a notification for empty List'

--- day1tutorial/src/Content.js
+++ day1tutorial/src/Content.js
@@ -39,27 +39,30 @@ const Content = () => {
   
     return (
         <main className="main">
-                <ul>
-                    {items.map((item)=>(
-                        <li className="item" key={item.id}>
-                            <input  type="checkbox" 
-                                    onChange={()=>handleCheck(item.id)}
-                                    checked={item.checked}
-                            />
-                            <label
-                            onDoubleClick={()=>handleCheck(item.id)}
-                            style={(item.checked) ? { textDecoration : 'line-through'} : null}
-                            
-                            >{item.item}</label>
-                            
-                            <BsTrash role="button"
-                                onClick={()=>handleDelete(item.id)}
-                             tabIndex="0"
-                             />
+            {items.length ? (
+                    <ul>
+                        {items.map((item)=>(
+                            <li className="item" key={item.id}>
+                                <input  type="checkbox" 
+                                        onChange={()=>handleCheck(item.id)}
+                                        checked={item.checked}
+                                />
+                                <label
+                                onDoubleClick={()=>handleCheck(item.id)}
+                                style={(item.checked) ? { textDecoration : 'line-through'} : null}
+                                
+                                >{item.item}</label>
+                                
+                                <BsTrash role="button"
+                                    onClick={()=>handleDelete(item.id)}
+                                tabIndex="0"
+                                />
 
-                        </li>
-                    ))}
-                </ul>
+                            </li>
+                        ))}
+                    </ul>
+            ) : ( <p style= {{marginTop: '2rem', color: 'red'}}>Your List is Empty</p>
+            )}        
         </main>
     );
 };
