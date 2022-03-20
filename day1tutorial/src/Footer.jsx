d019b1669588c483ddaeee8c12e961f42ab1613b
shawasty <shawasty@gmail.com>
2022-03-19 23:02:43 -0400

    studied some fundamentals

---
+++ day1tutorial/src/Footer.jsx
@@ -0,0 +1,13 @@
+
+
+const Footer = () => {
+    const today = new Date();
+    return (
+        <div className="footer">
+            <p> Copyright &copy; {today.getFullYear()}</p>
+            
+        </div>
+    );
+};
+
+export default Footer;
---
+++ day1tutorial/src/Header.jsx
@@ -0,0 +1,12 @@
+
+
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>Groceries List</h1>
+        </div>
+    );
+};
+
+export default Header;
