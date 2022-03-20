9e788143663734fdefaf892041a11e1efaa81111
shawasty <shawasty@gmail.com>
2022-03-20 01:07:10 -0400

    practiced abstraction

--- day1tutorial/src/Content.js
+++ day1tutorial/src/Content.js
@@ -1,31 +1,16 @@
 
-import { BsTrash } from "react-icons/bs"
+import ItemList from "./ItemList";
 
 const Content = ({items, handleCheck,handleDelete}) => {
     return (
         <main className="main">
             {items.length ? (
-                    <ul>
-                        {items.map((item)=>(
-                            <li className="item" key={item.id}>
-                                <input  type="checkbox" 
-                                        onChange={()=>handleCheck(item.id)}
-                                        checked={item.checked}
-                                />
-                                <label
-                                onDoubleClick={()=>handleCheck(item.id)}
-                                style={(item.checked) ? { textDecoration : 'line-through'} : null}
-                                
-                                >{item.item}</label>
-                                
-                                <BsTrash role="button"
-                                    onClick={()=>handleDelete(item.id)}
-                                tabIndex="0"
-                                />
-
-                            </li>
-                        ))}
-                    </ul>
+                    <ItemList
+                    items={items}
+                    handleCheck={handleCheck}
+                    handleDelete={handleDelete}
+                    
+                    />
             ) : ( <p style= {{marginTop: '2rem', color: 'red'}}>Your List is Empty</p>
             )}        
         </main>
